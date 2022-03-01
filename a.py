ad19fad3e4161fb2fb493a3ad4b4846b0a9967b7
Alireza Hajizadeh <68115766+Alireza-Hajizadeh@users.noreply.github.com>
2022-03-01 21:21:54 +0330

    Add files via upload

---
+++ a.py
@@ -0,0 +1,13 @@
+#Alireza-Hajizadeh
+#send whatsapp msg in python
+from pygame import init
+from WhatsAppmsg import sendwhatmsg_instantly
+from WhatsAppmsg import pg
+
+number = input("Enter your number (+989999999999) : ")
+msg = input("Enter your messeage : ")
+timeleft = input("Enter your Time left : ")
+
+sendwhatmsg_instantly(number , msg, timeleft)
+wait_time: int = 1500
+pg.press("enter")
